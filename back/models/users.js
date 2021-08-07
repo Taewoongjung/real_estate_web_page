48a9755156fa10a0b3442d8b97e7e4720a368b97
Taewoong <aipooh8882@naver.com>
2021-08-07 16:23:39 +0900

    user table completed

---
+++ back/models/users.js
@@ -0,0 +1,36 @@
+const sequelize = require('sequelize');
+
+module.exports = class Users extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init({
+            email: {
+                type: Sequelize.STRING(40),
+                allowNull: true,
+                unique: true,
+            },
+            name: {
+                type: Sequeliuze.STRING(17),
+                allowNull: true,
+            },
+            nick: {
+                type: Sequelize.STRING(17),
+                allowNull: true,
+            },
+            password: {
+                type: Sequelize.STRING(100),
+                allowNull: true,
+            }
+        }, {
+            sequelize,
+            timestamps: true,
+            paranoid: true,
+            modelName: 'User',
+            tableName: 'users',
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
+        });
+    }
+    // static associate(db) {
+    //     db.User.hasMany(db.Who);
+    // }
+}
