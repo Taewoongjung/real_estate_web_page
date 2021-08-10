c05c18b492ec9a42f97615d1a4d60f177984ef3d
Taewoong <aipooh8882@naver.com>
2021-08-11 04:01:33 +0900

    request using axios is completed and datas are well transfering

--- back/routes/api.js
+++ back/routes/api.js
@@ -7,15 +7,21 @@ const router = express.Router();
 
 router.get('/',  async(req, res, next) => {
     try{
+        const { pnu, stdrYear } = req.query;
+        console.log("query로 넘어오는 값들", pnu, stdrYear);
         const getItem = await axios.get(
+            `https://api.allorigins.win/get?url=
+            ${encodeURIComponent(
+                `http://apis.data.go.kr/1611000/nsdi/IndvdLandPriceService/attr/getIndvdLandPriceAttr?ServiceKey=${process.env.SERVICE_KEY}&pnu=${pnu}&stdrYear=${stdrYear}&format=json&numOfRows=1&pageNo=1`)}`
+        );
+        console.log(getItem.data.contents);
+        const jsonData = JSON.parse(getItem.data.contents);
+        console.log("@!!@!!!");
 
-            `http://apis.data.go.kr/1611000/nsdi/IndvdLandPriceService/attr/getIndvdLandPriceAttr?ServiceKey=${process.env.SERVICE_KEY}&pnu=1165010100109760014&stdrYear=2021&format=json`,
-            {
-                headers: {
-                    "Access-Control-Allow-Origin": "*",
-                }
-            });
-        console.log(getItem);
+        const All = jsonData.indvdLandPrices.field
+        console.log(All);
+
+        return res.send(All);
     } catch (error) {
         console.log(error);
     }
