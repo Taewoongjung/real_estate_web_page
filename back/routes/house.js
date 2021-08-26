a4b853708f14f2bc59826448e68f6bdd628396a4
Taewoong <aipooh8882@naver.com>
2021-08-26 21:52:03 +0900

    all information about apt mapped to the table

--- back/routes/house.js
+++ back/routes/house.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const axios = require('axios');
+// const parser = require('fast-xml-parser');
+
+const parser = require('xml2js');
+const iconvlite = require("iconv-lite");
+const convert = require('xml-js');
+const request = require('request');
+const xmlParser = require('xml2json');
+
 require('dotenv').config();
 
 const { Like } = require("../models");
@@ -7,13 +15,23 @@ const { Like } = require("../models");
 const router = express.Router();
 
 router.get('/', async(req, res, next) => {
-    const { address } = req.query;
-    console.log("(house) query로 넘어오는 값들", address);
-    const getItemFir = await axios.get(
-        `https://api.allorigins.win/get?url=
+    try {
+        const { address, bcode } = req.query;
+        console.log("(house) query로 넘어오는 값들", address, bcode);
+        const getItemFir = await axios.get(
+            `https://api.allorigins.win/get?url=
             ${encodeURIComponent(
-            `http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?LAWD_CD=11110&DEAL_YMD=201512&serviceKey=${process.env.SERVICE_KEY}`
-        )}`
-    );
+                `http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?LAWD_CD=${bcode}&DEAL_YMD=202107&pageNo=5&numOfRows=100&serviceKey=${process.env.SERVICE_KEY}`
+            )}`
+        )
+            .then((response) => {
+                // console.log(response.data.contents);
+                var resultString = response.data.contents;
+                console.log("result = ", xmlParser.toJson(resultString));
+                return res.send(xmlParser.toJson(resultString));
+            });
+    } catch (error) {
+        console.log(error);
+    }
 });
 module.exports = router;
