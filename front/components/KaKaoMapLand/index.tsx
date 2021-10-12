69ed7a8730c423bf6640848e129c6dc68a89f6a3
Taewoong <aipooh8882@naver.com>
2021-10-12 20:29:27 +0900

    comments removed

--- front/components/KaKaoMapLand/index.tsx
+++ front/components/KaKaoMapLand/index.tsx
@@ -250,9 +250,6 @@ const KaKaoMapLand: VFC = () => {
                 const digits_main = main.toString().split('');
                 const arr_main = new Array(4);
                 arr_main.fill(0);
-                //digits: ['1', '2', '3']
-                //digits.length=3   i:2->1->0, j:3->2->1
-                //arr=[0,0,0,0] arr[3]=3  arr[2]=2  arr[1]=1
                 for (let j=3, i=digits_main.length-1; i>=0; i--, j--) {
                     arr_main[j] = Number(digits_main[i]);
                 }
